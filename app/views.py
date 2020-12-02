026030479ddd272a0718efdb22a2e17ba9f2bf4d
mikengugy <mikengugy@gmail.com>
2020-12-02 16:04:50 +0300

    added forms

--- app/views.py
+++ app/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from .forms import SignUpForm, BusinessForm, NeighbourHoodForm, PostForm
+from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood, Business, Post 
+
 
 # Create your views here.
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def create_neighbourhood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})
+
+def neighbourhoods(request):
+    all_hoods = Neighbourhood.objects.all()
+    all_hoods = all_hoods[::-1]
+    params = {
+        'all_hoods': all_hoods,
+    }
+    return render(request, 'neighbourhoods.html', params)
+
+def create_post(request, hood_id):
+    hood = Neighbourhood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {'form': form})
+

9d8b8a27dbb42cab54669691733c31a63bbc3e13
joansaj <joannsaj@gmail.com>
2020-12-02 10:04:18 +0300

    registration and login of users

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+# class Profile(models.Model):
+#     profile = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+#     name = models.CharField(max_length=60)
+#     location = models.CharField(max_length=60)
+#     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='neighbourhood')
+#     profile_pic = CloudinaryField('image', null=True)
+#     bio = models.TextField(null=True)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        all_users = User.objects.all()
+        serializers = UserSerializer(all_users, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    

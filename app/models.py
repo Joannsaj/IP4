932e85af0a7eed114fe2d196f06e32aaf354d827
joansaj <joannsaj@gmail.com>
2020-12-04 07:19:35 +0300

    returned post and business

--- app/models.py
+++ app/models.py
@@ -2,8 +2,6 @@ from django.db import models
 import datetime as dt
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractUser
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -44,46 +42,47 @@ class Profile(models.Model):
     neighborhood = models.ForeignKey(Neighbourhood, on_delete= models.CASCADE, related_name='neighbourhood' )
     email_address = models.EmailField(max_length=60)
     location = models.CharField(max_length=60, null=True)
-    # @receiver(post_save, sender=User)
-    # def create_user_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         Profile.objects.create(user=instance)
+    
+    def __str__(self):
+        return f'{self.name} Profile'
 
-    # @receiver(post_save, sender=User)
-    # def save_user_profile(sender, instance, **kwargs):
-    #     instance.profile.save()    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+            
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, related_name='hood_post')
 
 
-# class Business(models.Model):
-#     name = models.CharField(max_length=120)
-#     email = models.EmailField(max_length=254)
-#     description = models.TextField(blank=True)
-#     neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, related_name='business')
-#     user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='owner')
+    def __str__(self):
+        return f'{self.title} Post'
 
-#     def __str__(self):
-#         return f'{self.name} Business'
+    def save_post(self):
+        self.save()
 
-#     def save_business(self):
-#         self.save()
-
-#     def delete_business(self):
-#         self.delete()
-
-# class Post(models.Model):
-#     title = models.CharField(max_length=120, null=True)
-#     post = models.TextField()
-#     date = models.DateTimeField(auto_now_add=True)
-#     user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='post_owner')
-#     hood = models.ForeignKey('Neighbourhood', on_delete=models.CASCADE, related_name='hood_post')
-
-
-#     def __str__(self):
-#         return f'{self.title} Post'
-
-#     def save_post(self):
-#         self.save()
-
-#     def delete_post(self):
-#         self.delete()
+    def delete_post(self):
+        self.delete()
             

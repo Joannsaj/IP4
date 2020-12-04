c44daf7aa69a8a761ec38e1a8cd3d588048f6144
joansaj <joannsaj@gmail.com>
2020-12-04 07:25:06 +0300

    added post and business serializer views

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect,render #, get_object_or_404
 # from django.contrib.auth import login, authenticate
 # from .forms import  BusinessForm, NeighbourHoodForm, PostForm
 # from django.contrib.auth.decorators import login_required
-from .models import Neighbourhood, CustomUser , Profile #Business, Post , 
+from .models import Neighbourhood, CustomUser , Profile ,Business, Post 
 from rest_framework import generics, viewsets
 from . import models
 from . import serializers
@@ -32,56 +32,12 @@ class ProfileSerializer(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = serializers.ProfileSerializer    
 
-# @login_required(login_url='login')
-# def index(request):
-#     return render(request, 'index.html')
+class BusinessSerializer(viewsets.ModelViewSet):
+    queryset = Business.objects.all()
+    serializer_class = serializers.BusinessSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
-
-# def create_neighbourhood(request):
-#     if request.method == 'POST':
-#         form = NeighbourHoodForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             hood = form.save(commit=False)
-#             hood.admin = request.user.profile
-#             hood.save()
-#             return redirect('hood')
-#     else:
-#         form = NeighbourHoodForm()
-#     return render(request, 'newhood.html', {'form': form})
-
-# def neighbourhoods(request):
-#     all_hoods = Neighbourhood.objects.all()
-#     all_hoods = all_hoods[::-1]
-#     params = {
-#         'all_hoods': all_hoods,
-#     }
-#     return render(request, 'neighbourhoods.html', params)
-
-# def create_post(request, hood_id):
-#     hood = Neighbourhood.objects.get(id=hood_id)
-#     if request.method == 'POST':
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.hood = hood
-#             post.user = request.user.profile
-#             post.save()
-#             return redirect('single-hood', hood.id)
-#     else:
-#         form = PostForm()
-#     return render(request, 'post.html', {'form': form})
-
-
-
-# class UserList(APIView):
-#     def get(self, request, format=None):
-#         all_users = User.objects.all()
-#         serializers = UserSerializer(all_users, many=True)
-#         return Response(serializers.data)
-
-#     def post(self, request, format=None):
-#         serializers = UserSerializer(data=request.data)
-#         if serializers.is_valid():
-#             serializers.save()
-#             return Response(serializers.data, status=status.HTTP_201_CREATED)
-#         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
+class PostSerializer(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer    
+    permission_classes = (IsAdminOrReadOnly,)
